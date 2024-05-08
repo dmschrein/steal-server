8d45265ae5d5750611387c4c5883645748492be3
dmschrein <darrelmschreiner@gmail.com>
2024-05-08 08:47:15 -0700

    added joinsteal domains

--- config/allowedOrigins.js
+++ config/allowedOrigins.js
@@ -1,7 +1,8 @@
 const allowedOrigins = [
   'http://localhost:5173',
-  'https://www.risingfounder.io',
-  'https://risingfounder.io'
+  'https://www.joinsteal.com',
+  'https://joinsteal.com',
+  'https://app.netlify.com/sites/venerable-sprite-27eb65'
 ];
 
 export default allowedOrigins;
